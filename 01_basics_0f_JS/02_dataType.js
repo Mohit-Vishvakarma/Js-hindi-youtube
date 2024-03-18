a4af312898faef46d305dc841a5a61efc48f97ca
Mohit-Vishvakarma <150326877+Mohit-Vishvakarma@users.noreply.github.com>
2024-03-18 09:08:52 +0000

    data type and conversion basics

--- 01_basics_0f_JS/02_dataType.js
+++ 01_basics_0f_JS/02_dataType.js
@@ -3,3 +3,11 @@
 
 // alert(3 + 3)  
 // This code is run on browser but not run in node becuse the engine of JS is in browser not in node
+
+
+let age = null   // type is object
+let name = undefined  // type is undefined
+console.log(typeof age); // typeof use to cheak type of data.
+
+
+// 
---
+++ 01_basics_0f_JS/03_ConversionOperation.js
@@ -0,0 +1,25 @@
+// Conversion in JavaScript 
+
+// let score = "33";
+let score = null  
+
+
+let scoreIntoNum = Number(score)  // use Numbe() to change data type
+console.log(typeof scoreIntoNum); // use typeof to cheak data type 
+
+// let isLoggedIn2 =  0 // Every number return true value but 0 return false value
+let isLoggedIn =  "Mohit" // Empty string return false value and string with value return true value
+let isLoggedInIntoBoolean = Boolean(isLoggedIn)
+console.log(isLoggedInIntoBoolean);
+console.log(typeof isLoggedInIntoBoolean);
+
+
+// Boolean return the value
+
+//  ""  => false
+// "Mohit"   => true 
+// 0  => false
+// 1 => true
+
+
+
