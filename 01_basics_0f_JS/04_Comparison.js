f0d22350213b6ba368e48437a3d4bff72913d3fc
Mohit-Vishvakarma <150326877+Mohit-Vishvakarma@users.noreply.github.com>
2024-03-18 09:50:36 +0000

    comparision by null undefined and other comparetion

---
+++ 01_basics_0f_JS/04_Comparison.js
@@ -0,0 +1,10 @@
+console.log("2" > 1); // return ture becuse JS apply conversion and converte "2" to 2 and 2 > 1
+
+console.log(null > 0) // the value of null is not defined So the condition is false
+console.log(null == 0) // here JS Converte of null into 0. but condition is false
+console.log(null >= 0) // becuse JS Converte of null into 0. So the condition is true
+
+
+console.log("2" == 2); // return true because == cheak only value not data type (in == JS is use conversion)
+console.log("2" === 2); // return false because === cheak data type also (in === JS is not use conversion)
+
