87394a820e17e227f8f96457bd75867213484541
Mohit-Vishvakarma <150326877+Mohit-Vishvakarma@users.noreply.github.com>
2024-03-24 02:16:51 +0000

    date property and concept

---
+++ 01_basics_0f_JS/10_data_time.js
@@ -0,0 +1,26 @@
+// *************Date********************* 
+
+let myDate = new Date();
+
+// way to defined the date
+
+console.log(myDate);
+console.log(myDate.toString());                 
+console.log(myDate.toLocaleString());
+console.log(myDate.toJSON());
+
+// second way 
+// let newDate = new Date(2024, 3, 12)
+// console.log(newDate.toDateString());
+
+// let newDate = new Date(2024, 3, 12, 5, 9)
+// let newDate = new Date("2024-01-16")
+let newDate = new Date("01-18-2024")
+
+console.log(newDate.toLocaleDateString());
+
+
+
+
+// ********************** time *************************
+
